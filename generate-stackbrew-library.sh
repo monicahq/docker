941d92e8da02916ca9df30f772443dd2b836292e
Alexis Saettler <alexis@saettler.org>
2020-02-28 12:30:41 +0100

    First version of monica image

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+set -euo pipefail
+
+self="$(basename "$BASH_SOURCE")"
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+defaultVariant='apache'
+
+# Get the most recent commit which modified any of "$@".
+fileCommit() {
+	git log -1 --format='format:%H' HEAD -- "$@"
+}
+
+# Get the most recent commit which modified "$1/Dockerfile" or any file that
+# the Dockerfile copies into the rootfs (with COPY).
+dockerfileCommit() {
+	local dir="$1"; shift
+	(
+		cd "$dir";
+		fileCommit Dockerfile \
+			$(git show HEAD:./Dockerfile | awk '
+				toupper($1) == "COPY" {
+					for (i = 2; i < NF; i++)
+							print $i;
+				}
+			')
+	)
+}
+
+getArches() {
+	local repo="$1"; shift
+	local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/'
+
+	eval "declare -g -A parentRepoToArches=( $(
+		find -name 'Dockerfile' -exec awk '
+				toupper($1) == "FROM" && $2 !~ /^('"$repo"'|scratch|microsoft\/[^:]+)(:|$)/ {
+					print "'"$officialImagesUrl"'" $2
+				}
+			' '{}' + \
+			| sort -u \
+			| xargs bashbrew cat --format '[{{ .RepoName }}:{{ .TagName }}]="{{ join " " .TagEntry.Architectures }}"'
+	) )"
+}
+getArches 'postfixadmin'
+
+# Header.
+cat <<-EOH
+# This file is generated via https://github.com/monicahq/docker/blob/$(fileCommit "$self")/$self
+Maintainers: Alexis Saettler <alexis@saettler.org>
+GitRepo: https://github.com/monicahq/docker.git
+EOH
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
+
+SEARCHFOR="Upgrade available - the latest version is "
+latest="$(curl -fsSL 'https://api.github.com/repos/monicahq/monica/releases/latest' | jq -r '.tag_name')"
+
+variants=( */ )
+variants=( "${variants[@]%/}" )
+
+for variant in "${variants[@]}"; do
+	commit="$(dockerfileCommit "$variant")"
+	fullversion="$(git show "$commit":"$variant/Dockerfile" | grep -iF "ARG MONICA_VERSION" | sed -E "s@ARG MONICA_VERSION=([0-9.]+)@\1@")"
+
+	versionAliases=( "$fullversion" "${fullversion%.*}" "${fullversion%.*.*}" )
+	if [ "$fullversion" = "$latest" ]; then
+		versionAliases+=( "latest" )
+	fi
+
+	variantAliases=( "${versionAliases[@]/%/-$variant}" )
+	variantAliases=( "${variantAliases[@]//latest-}" )
+
+	if [ "$variant" = "$defaultVariant" ]; then
+		variantAliases+=( "${versionAliases[@]}" )
+	fi
+
+	variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$variant/Dockerfile")"
+	variantArches="${parentRepoToArches[$variantParent]}"
+
+	cat <<-EOE
+
+		Tags: $(join ', ' "${variantAliases[@]}")
+		Architectures: $(join ', ' $variantArches)
+		Directory: $variant
+		GitCommit: $commit
+	EOE
+done
---
+++ update.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -e
+
+IFS='
+'
+
+declare -A php_version=(
+	[default]='7.3'
+)
+
+declare -A cmd=(
+	[apache]='apache2-foreground'
+	[fpm]='php-fpm'
+	[fpm-alpine]='php-fpm'
+)
+
+declare -A base=(
+	[apache]='debian'
+	[fpm]='debian'
+	[fpm-alpine]='alpine'
+)
+
+declare -A document=(
+	[apache]="ENV APACHE_DOCUMENT_ROOT /var/www/html/public\\n\
+RUN set -eu; sed -ri -e \"s!/var/www/html!\\\${APACHE_DOCUMENT_ROOT}!g\" /etc/apache2/sites-available/*.conf; \\\\\\n\
+	sed -ri -e \"s!/var/www/!\\\${APACHE_DOCUMENT_ROOT}!g\" /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf"
+	[fpm]=''
+	[fpm-alpine]=''
+)
+
+apcu_version="$(
+	git ls-remote --tags https://github.com/krakjoe/apcu.git \
+		| cut -d/ -f3 \
+		| grep -vE -- '-rc|-b' \
+		| sed -E 's/^v//' \
+		| sort -V \
+		| tail -1
+)"
+
+memcached_version="$(
+	git ls-remote --tags https://github.com/php-memcached-dev/php-memcached.git \
+		| cut -d/ -f3 \
+		| grep -vE -- '-rc|-b' \
+		| sed -E 's/^[rv]//' \
+		| sort -V \
+		| tail -1
+)"
+
+redis_version="$(
+	git ls-remote --tags https://github.com/phpredis/phpredis.git \
+		| cut -d/ -f3 \
+		| grep -viE '[a-z]' \
+		| tr -d '^{}' \
+		| sort -V \
+		| tail -1
+)"
+
+declare -A pecl_versions=(
+	[APCu]="$apcu_version"
+	[memcached]="$memcached_version"
+	[redis]="$redis_version"
+)
+
+version="$(curl -fsSL 'https://api.github.com/repos/monicahq/monica/releases/latest' | jq -r '.tag_name')"
+sha512="$(curl -fsSL "https://github.com/monicahq/monica/releases/download/$version/monica-$version.sha512" | grep monica-$version.tar.bz2 | awk '{ print $1 }')"
+
+set -x
+
+for variant in apache fpm fpm-alpine; do
+	rm -rf $variant
+	mkdir -p $variant
+	phpVersion=${php_version[$version]-${php_version[default]}}
+
+	template="Dockerfile-${base[$variant]}.template"
+	sed -e '
+		s/%%VARIANT%%/'"$variant"'/;
+		s/%%PHP_VERSION%%/'"$phpVersion"'/;
+		s/%%VERSION%%/'"$version"'/;
+		s/%%SHA512%%/'"$sha512"'/;
+		s/%%CMD%%/'"${cmd[$variant]}"'/;
+		s#%%APACHE_DOCUMENT%%#'"${document[$variant]}"'#;
+		s/%%APCU_VERSION%%/'"${pecl_versions[APCu]}"'/;
+		s/%%MEMCACHED_VERSION%%/'"${pecl_versions[memcached]}"'/;
+		s/%%REDIS_VERSION%%/'"${pecl_versions[redis]}"'/;
+	' \
+		$template > "$variant/Dockerfile"
+	
+	for file in entrypoint cron queue; do
+		cp docker-$file.sh $variant/$file.sh
+	done
+	
+	cp upgrade.exclude $variant/
+done

8118ac0f51564c16d68131dae301b7151d323a39
Alexis Saettler <alexis@saettler.org>
2021-06-22 23:42:00 +0200

    New entrypoint + v3.1.0 (#59)

--- apache/entrypoint.sh
+++ apache/entrypoint.sh
@@ -4,26 +4,63 @@ set -Eeo pipefail
 
 # wait for the database to start
 waitfordb() {
-    HOST=${DB_HOST:-mysql}
-    PORT=${DB_PORT:-3306}
-    echo "Connecting to ${HOST}:${PORT}"
+    TERM=dumb php -- <<'EOPHP'
+<?php
+function env(string $name, ?string $default = null): ?string
+{
+    $val = getenv($name);
+    return $val === false ? $default : $val;
+}
 
-    attempts=0
-    max_attempts=30
-    while [ $attempts -lt $max_attempts ]; do
-        busybox nc -w 1 "${HOST}:${PORT}" && break
-        echo "Waiting for ${HOST}:${PORT}..."
-        sleep 1
-        let "attempts=attempts+1"
-    done
+$stderr = fopen('php://stderr', 'w');
 
-    if [ $attempts -eq $max_attempts ]; then
-        echo "Unable to contact your database at ${HOST}:${PORT}"
-        exit 1
-    fi
+if (! env('DATABASE_URL')) {
+    $host = env('DB_HOST', '127.0.0.1');
+    $port = (int) env('DB_PORT', '3306');
+    $user = env('DB_USERNAME', 'homestead');
+    $pass = env('DB_PASSWORD', 'secret');
+    $database = env('DB_DATABASE', 'monica');
+    $socket = env('DB_UNIX_SOCKET');
+} else {
+    $url = parse_url(env('DATABASE_URL'));
+    $host = $url['host'];
+    $port = array_key_exists('port', $url) ? (int) $url['port'] : 0;
+    $user = $url['user'];
+    $pass = $url['pass'];
+    $database = ltrim($url['path'], '/');
+    $socket = null;
+    if ($url['query'] && strpos($url['query'], 'unix_socket=') !== false) {
+        $socket = substr($url['query'], strlen('unix_socket='));
+    }
+}
 
-    echo "Waiting for database to settle..."
-    sleep 3
+$collation = ((bool) env('DB_USE_UTF8MB4', true)) ? ['utf8mb4','utf8mb4_unicode_ci'] : ['utf8','utf8_unicode_ci'];
+
+$maxAttempts = 30;
+do {
+    $mysql = new mysqli($host, $user, $pass, '', $port, $socket);
+    if ($mysql->connect_error) {
+        fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
+        --$maxAttempts;
+        if ($maxAttempts <= 0) {
+            fwrite($stderr, "\n" . 'Unable to contact your database');
+            $mysql->close();
+            exit(1);
+        }
+        fwrite($stderr, "\n" . 'Waiting for database to settle...');
+        sleep(1);
+    }
+} while ($mysql->connect_error);
+fwrite($stderr, "\n" . 'Database ready.');
+
+if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($database) . '` CHARACTER SET ' . $collation[0] . ' COLLATE ' . $collation[1])) {
+    fwrite($stderr, "\n" . 'MySQL "CREATE DATABASE" Error: ' . $mysql->error . "\n");
+    $mysql->close();
+    exit(1);
+}
+
+$mysql->close();
+EOPHP
 }
 
 if expr "$1" : "apache" 1>/dev/null || [ "$1" = "php-fpm" ]; then
